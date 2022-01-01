09060a0e9b5931d621c91871b0b90d08a06d5a23
Fernando Ruiz <fernando@frudev.es>
2022-01-01 20:37:34 +0100

    configure api/users

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,13 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 // import { sampleUserData } from '../../../utils/sample-data'
 
-const handler = (_req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
   try {
-    if (!Array.isArray([])) {
-      throw new Error("Cannot find user data");
-    }
-
-    res.status(200).json([]);
+    const response = await fetch("https://gorest.co.in/public/v1/users");
+    const data = await response.json();
+    const users = data["data"];
+    res.status(200).json(users);
   } catch (err) {
     res.status(500).json({ statusCode: 500, message: err.message });
   }
